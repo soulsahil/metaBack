6ffe28e98acaadc23ced4219450a70321b1f1445
soulsahil <92634234+soulsahil@users.noreply.github.com>
2025-04-04 11:37:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const PORT = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGO_CONN, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  tls: true
+  tls: true,
+  tlsInsecure: true
 });
 
 // Middleware
